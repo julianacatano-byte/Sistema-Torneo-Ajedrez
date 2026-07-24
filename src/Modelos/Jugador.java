217d8e73bf653e8ebdf7dc6b4e2edfaee07d08e3
juliana <juliana.catano@autonoma.edu.co>
2026-07-24 14:23:44 -0500

    realice en el paquete de modelos la clase jugador y la clase partida, con sus respectivos atributos, constructores, getters y setters

--- src/Modelos/Jugador.java
+++ src/Modelos/Jugador.java
@@ -1,4 +1,32 @@
 package Modelos;
 
 public class Jugador {
+    private String nombre;
+    private String cedula;
+    private int puntaje;
+
+public Jugador(String nombre, String cedula) {
+    this.nombre = nombre;
+    this.cedula = cedula;
+    this.puntaje = 0;
 }
+
+        public String getNombre () {
+            return nombre;
+        }
+        public void setNombre (String nombre){
+            this.nombre = nombre;
+        }
+        public String getCedula () {
+            return cedula;
+        }
+        public void setCedula (String cedula){
+            this.cedula = cedula;
+        }
+        public int getPuntaje () {
+            return puntaje;
+        }
+        public void setPuntaje ( int puntaje){
+            this.puntaje = puntaje;
+        }
+    }
--- src/Modelos/Partida.java
+++ src/Modelos/Partida.java
@@ -1,4 +1,47 @@
 package Modelos;
 
 public class Partida {
+    private int idPartida;
+    private Jugador jugadorConPiezasBlancas;
+    private Jugador jugadorConPiezasNegras;
+    private String resultado;
+
+    public Partida(int idPartida, Jugador JugadorConPiezasBlancas, Jugador JugadorConPiezasNegras, String resultado) {
+        this.idPartida = idPartida;
+        this.jugadorConPiezasBlancas = JugadorConPiezasBlancas;
+        this.jugadorConPiezasNegras = JugadorConPiezasNegras;
+        this.resultado = resultado;
+    }
+
+    public int getidPartida() {
+        return idPartida;
+    }
+
+    public void setidPartida(int idPartida) {
+        this.idPartida = idPartida;
+    }
+
+    public Jugador getJugadorConPiezasBlancas() {
+        return jugadorConPiezasBlancas;
+    }
+
+    public void setJugadorConPiezasBlancas(Jugador jugadorConPiezasBlancas) {
+        this.jugadorConPiezasBlancas = jugadorConPiezasBlancas;
+    }
+
+    public Jugador getJugadorConPiezasNegras() {
+        return jugadorConPiezasNegras;
+    }
+
+    public void setJugadorConPiezasNegras(Jugador jugadorConPiezasNegras) {
+        this.jugadorConPiezasNegras = jugadorConPiezasNegras;
+    }
+
+    public String getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(String resultado) {
+        this.resultado = resultado;
+    }
 }
