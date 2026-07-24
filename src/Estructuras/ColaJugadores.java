84116de50c7765387674797a1c20e79dd27c3177
juliana <juliana.catano@autonoma.edu.co>
2026-07-24 14:58:25 -0500

    realice la clase ColaJugadores y NodoCola, para gestionar la lista de espera de los jugadores

--- src/Estructuras/ColaJugadores.java
+++ src/Estructuras/ColaJugadores.java
@@ -1,4 +1,38 @@
 package Estructuras;
 
+import Modelos.Jugador;
+
 public class ColaJugadores {
+    private NodoCola frente;
+    private NodoCola finalCola;
+
+
+    public ColaJugadores() {
+        frente = null;
+        finalCola = null;
+    }
+    public void encolar(Jugador jugador) {
+        NodoCola nuevoNodo = new NodoCola(jugador);
+        if (estaVacia()) {
+            frente = nuevoNodo;
+            finalCola = nuevoNodo;
+        } else {
+            finalCola.setSiguiente(nuevoNodo);
+            finalCola = nuevoNodo;
+        }
+    }
+    public boolean estaVacia() {
+        return frente == null;
+    }
+    public Jugador desencolar() {
+        if (estaVacia()) {
+            return null;
+        }
+        Jugador jugador = frente.getJugador();
+        frente = frente.getSiguiente();
+        if (frente == null) {
+            finalCola = null;
+        }
+        return jugador;
+    }
 }
--- src/Estructuras/NodoCola.java
+++ src/Estructuras/NodoCola.java
@@ -1,4 +1,25 @@
 package Estructuras;
 
+import Modelos.Jugador;
+
 public class NodoCola {
+    private Jugador jugador;
+    private NodoCola siguiente;
+
+    public NodoCola(Jugador jugador) {
+        this.jugador = jugador;
+        this.siguiente = null;
+    }
+    public Jugador getJugador() {
+        return jugador;
+    }
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
+    public NodoCola getSiguiente() {
+        return siguiente;
+    }
+    public void setSiguiente(NodoCola siguiente) {
+        this.siguiente = siguiente;
+    }
 }
